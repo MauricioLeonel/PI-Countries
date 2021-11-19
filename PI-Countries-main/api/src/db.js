1b7b869c04bc194a3ca0998bd0e26c45ebbda93a
MauricioLeonel <85882961+MauricioLeonel@users.noreply.github.com>
2021-11-18 21:19:31 -0300

    mando cambios pi

--- PI-Countries-main/api/src/db.js
+++ PI-Countries-main/api/src/db.js
@@ -2,13 +2,24 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+// const pg= require('pg')
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_USER_HERO,DB_PASSWORD_HERO,DB_HOST_HERO,DB_DB_HERO
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/homework`, {
+
+
+//client.connect(`postgres://${DB_USER_HERO}:${DB_PASSWORD_HERO}@${DB_HOST_HERO}/${DB_DB_HERO}`)
+
+const sequelize = new Sequelize(`postgres://${DB_USER_HERO}:${DB_PASSWORD_HERO}@${DB_HOST_HERO}/${DB_DB_HERO}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+     },
 });
 const basename = path.basename(__filename);
 
